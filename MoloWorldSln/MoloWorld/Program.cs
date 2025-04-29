2728f80a9757a36f9e1cde09b3038016d5fe269f
Makhi7 <mvmangxola22@gmail.com>
2025-04-29 13:34:36 +0200

    Retrieve sentence from user

--- MoloWorldSln/MoloWorld/Program.cs
+++ MoloWorldSln/MoloWorld/Program.cs
@@ -5,7 +5,10 @@
         static void Main()
         {
             Console.WriteLine("Please kindly insert a sentence");
-            
+            string userSentence = Console.ReadLine();
+
+            int numberOfWords = LanguageFunctions.GetWordCountInSentence(userSentence);
+            Console.WriteLine($"The amount of words in the sentence is {numberOfWords} .");
         }
     }
 }
